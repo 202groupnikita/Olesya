dee29adbcc3526861fce883d05412983ccc84faa
olesyaaw <belovaolesyaaa@yandex.ru>
2025-10-28 19:36:39 +0300

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import olga from './images/olga.jpg'
+import olga from './images/olga.jpg';
+import number from "./images/number.png";
+import whatsapp from "./images/whatsapp.png";
+import telegram from "./images/telegram.png"
 import './App.css';
 
 function App() {
@@ -14,6 +17,12 @@ function App() {
           <p>Меня зовут Ольга, и я шеф-повар кейтеринга Банкетные традиции. Моя цель – создавать не просто еду, а гастрономический опыт, который запомнится вашим гостям.</p>
         </div>
            <img src={olga} alt="Olga" className='photo'></img>        
+      </div>
+      <div className='header-down'>
+        <text> Контакты </text>
+        <img src={number} alt="number" className='num'></img>
+        <img src={whatsapp} alt="whatsapp" className='wha'></img>
+        <img src={telegram} alt="telegram" className='tel'></img>
       </div>
     </div>
   );
